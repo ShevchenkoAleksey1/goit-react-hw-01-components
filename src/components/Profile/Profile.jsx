4e7027b1d847634923ee4aed92730293073dd018
VitaPetrushyna <alexres87@gmail.com>
2022-09-22 22:47:29 +0300

    profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+export const Profile = ({
+  user: {
+    username,
+    tag,
+    location,
+    avatar = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+    stats,
+  },
+}) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
